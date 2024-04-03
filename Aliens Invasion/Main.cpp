f75978cec3c89da57eac0269105f2164d63fc8d9
Fadysamy101 <151401360+Fadysamy101@users.noreply.github.com>
2024-04-04 00:51:22 +0200

    Add files via upload

---
+++ Aliens Invasion/Main.cpp
@@ -0,0 +1,10 @@
+#include"earthArmy.h"
+#include"alienArmy.h"
+#pragma once
+#include<iostream>
+
+int main()
+{
+	earthArmy u1(678, "Soldier", 2, 45, 25, 3);
+	alienArmy u2(979, "Asoldier", 4, 100, 30, 3);
+}
---
+++ Aliens Invasion/earthArmy.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class earthArmy
+{
+private:
+	static int EUnitsNO;
+	int ID;
+	string type;
+	int JoinTime;
+	int Health;
+	int Power;
+	int AttackCap;
+public:
+	earthArmy(int id, string t, int jt, int h, int p, int ac)
+	{
+		setID(id);
+		setAttCap(ac);
+		setType(t);
+		setHealth(h);
+		setJoinTime(jt);
+		setPower(p);
+		EUnitsNO++;
+	}
+	
+	
+	void setID(int id)
+	{
+		ID = id;
+	}
+	void setType(string s)
+	{
+		type = s;
+	}
+	void setJoinTime(int jt)
+	{
+		JoinTime=jt;
+	}
+	void setPower(int p)
+	{
+		Power = p;
+	}
+	void setHealth(int h)
+	{
+		if (h <= 100 && h>0)
+			Health = h;
+	}
+	void setAttCap(int ac)
+	{
+		AttackCap = ac;
+	}
+	
+	
+	int getID()
+	{
+		return ID;
+	}
+	string getType()
+	{
+		return type;
+	}
+	int getHealth()
+	{
+		return Health;
+	}
+	int getAttCap()
+	{
+		return AttackCap;
+	}
+	int getPower()
+	{
+		return Power;
+	}
+	int getJoinTime()
+	{
+		return JoinTime;
+	}
+};
+
+int earthArmy::EUnitsNO = 0;
